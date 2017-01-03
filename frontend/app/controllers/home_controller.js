c87a73ddb9aeebe741c2ec4b701cf1125ced845f
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-01-02 18:43:31 -0800

    Added comment to show controller purpose.

--- frontend/app/controllers/home_controller.js
+++ frontend/app/controllers/home_controller.js
@@ -2,5 +2,6 @@
 
 module.exports = function(app) {
   app.controller('HomeController', [function() {
+    // planning to introduce code here. may change controller name to BlogController
   }]);
 };
